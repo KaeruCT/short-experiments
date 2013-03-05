47d6a4de965193f911cafa98a8ca3e4df7a91b32
Andres V <darklink898@gmail.com>
2013-03-04 21:15:00 -0600

    more fun0

--- lel/a.js
+++ lel/a.js
@@ -3,8 +3,11 @@
     canvas,
     ctx,
     i = 0,
-    rate = 0.05,
+    j = 0,
+    rate = 0.01,
+    size = 1,
     color,
+    clear = true,
 
     init = function (c) {
         var self = this;
@@ -13,20 +16,28 @@
         ctx = canvas.getContext('2d');
         self.change_color(canvas.width/2);
 
-        ctx.draw_circle = function (x, y, r) {
+        ctx.draw_point = function (x, y) {
             ctx.beginPath();
-            ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+            ctx.arc(x, y, 1, 0, 2 * Math.PI, false);
             ctx.fill();
         };
     },
 
+    toggle_clear = function() {
+        clear = !clear;
+    },
+
     change_rate = function (r) {
-        rate = ((r/canvas.height)) * 0.025;
+        var f = canvas.width * 0.01;
+        rate = f/r;
     },
 
     change_color = function (c) {
+        var c2 = canvas.height/2;
+        size = Math.abs(c-c2)/c2;
+
         color = {
-            h: Math.floor(c/canvas.width * 360),
+            h: Math.floor(c/canvas.height * 360),
             s: '50%',
             l: '50%'
         };
@@ -46,13 +57,10 @@
         ctx.save();
 
         ctx.setTransform(1, 0, 0, 1, 0, 0);
-        //if ((new Date()).getSeconds() % 2 == 0) {
-        //    ctx.clearRect(0, 0, canvas.width, canvas.height);
-        //}
-        //ctx.fillStyle = '#000';
-        //ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        i += rate;
+        if (clear) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
 
         draw_stuff();
 
@@ -60,29 +68,31 @@
     },
 
     draw_stuff = function () {
-        var x, y, j, k, l,
-            circle_radius = 2,
-            yoff = circle_radius,
-            h = canvas.height - yoff*2,
-            inc = canvas.height;
+        var x, y, l = 0, k = 0,
+            ch = rate,
+            w = canvas.width,
+            h = canvas.height,
+            h2 = h/2,
+            inc = 8;
 
         ctx.fillStyle = fill_color();
 
-        for (j = 0; j < canvas.width; j += circle_radius) {
-            k = j*inc;
-
-            y = yoff + h / 2 + (Math.sin(i+k) * h / 2);
-            x = j;
-
-            ctx.draw_circle(x, y, circle_radius);
+        for (j = 0; j < w; j += 1) {
+            ctx.draw_point(
+                j,
+                h2 + size * (Math.sin((j+i) * ch) * h2)
+            );
         }
+
+        i += inc;
     };
 
     window.tabcin = {
         init: init,
         draw: draw,
         change_color: change_color,
-        change_rate: change_rate
+        change_rate: change_rate,
+        toggle_clear: toggle_clear
     };
 }(window));
 
