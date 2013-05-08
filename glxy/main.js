bbd0633289fb026a3e2ee97e9e495e0a3cf7312a
Andres V <darklink898@gmail.com>
2013-05-07 18:27:14 -0600

    more options

--- glxy/main.js
+++ glxy/main.js
@@ -8,7 +8,8 @@ function (callback) {
 	window.setTimeout(callback, 1000 / 60);
 };
 
-var canvas, ctx,
+var canvas, ctx, ctx2,
+	partcount,
 
 	pmin = 2,
 	G = 0.0003,
@@ -19,7 +20,11 @@ var canvas, ctx,
 	particles = [],
 
 	mdown = false, prevx = 0, prevy = 0,
-	initx = 0, inity = 0;
+	initx = 0, inity = 0,
+
+	pause = false,
+	trails = false
+	;
 
 function Particle(p) {
 	p = p || {};
@@ -32,6 +37,52 @@ function Particle(p) {
 	this.update = function () {
 		this.x += this.dx;
 		this.y += this.dy;
+	}
+}
+
+function addParticle(p) {
+	particles.push(p);
+	partcount.innerText = particles.length;
+}
+
+function clear(ctx) {
+	ctx.save();
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.restore();
+}
+
+function keydown(e) {
+	switch(e.keyCode) {
+	case 82: //R
+		particles = [];
+		partcount.innerText = 0;
+		clear(ctx2);
+		break;
+	case 72: //H
+		var help = document.getElementById('help'),
+			d = help.style.display;
+
+		help.style.display = (d == '' || d == 'block' ? 'none' : 'block');
+		break;
+	case 80: //P
+		pause = !pause;
+		break;
+	case 81: //Q
+		var i = 0, p;
+		for (; i < 10; i += 1) {
+			p = new Particle({
+				x: Math.random() * canvas.width,
+				y: Math.random() * canvas.height,
+				r: pmin + Math.random() * 4
+			});
+			addParticle(p);
+		}
+		break;
+	case 84: //T
+		trails = !trails;
+		clear(ctx2);
+		break;
 	}
 }
 
@@ -53,7 +104,7 @@ function mousedown (e) {
 
 function mouseup (e) {
 	var p = new Particle(mousep);
-	particles.push(p);
+	addParticle(p);
 	mdown = false;
 }
 
@@ -72,34 +123,35 @@ function mousemove(e) {
 function drawLoop () {
 	var p, p2,
 		dx, dy, dr,
+		cx = 0, cy = 0,
 		k = j = i = 0;
 
-	// physics magic
-	for (; j < particles.length; j += 1) {
-		p = particles[j];
-		for (k = 0; k < particles.length; k += 1) {
-			if (j !== k) {
-				p2 = particles[k];
-				dx = p.x - p2.x;
-				dy = p.y - p2.y;
-				dr = p2.r / Math.pow(p.r, 2) * G;
+	if (!pause) {
+		// physics magic
+		for (; j < particles.length; j += 1) {
+			p = particles[j];
+			for (k = 0; k < particles.length; k += 1) {
+				if (j !== k) {
+					p2 = particles[k];
+					dx = p.x - p2.x;
+					dy = p.y - p2.y;
+					dr = Math.sqrt(p2.r) / Math.pow(p.r, 2) * G;
 
-				p.dx -= dx*dr;
-				p.dy -= dy*dr;
+					p.dx -= dx*dr;
+					p.dy -= dy*dr;
+				}
 			}
+			p.update();
 		}
-		p.update();
 	}
 
 	// drawing magic
-    ctx.save();
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.restore();
+    clear(ctx);
 
     p = mousep;
 
     do {
+		// particle
         ctx.beginPath();
         ctx.arc(
             p.x,
@@ -108,10 +160,20 @@ function drawLoop () {
             0,
             6.28);
         ctx.fill();
+
+		if (i > 0 && trails) {
+			// trail
+			ctx2.beginPath();
+			ctx2.arc(p.x, p.y, Math.sqrt(Math.sqrt(p.r)), 0, 6.28);
+			ctx2.fill();
+		}
+
         p = particles[i++];
+
     } while (i <= particles.length);
 
 	if (mdown) {
+		// draw line
 		ctx.beginPath();
 		ctx.moveTo(initx, inity);
 		ctx.lineTo(prevx, prevy);
@@ -122,13 +184,22 @@ function drawLoop () {
 }
 
 window.onload = function () {
+	var c2 = document.getElementById('trail-canvas');
     canvas = document.getElementById('main-canvas');
+    partcount = document.getElementById('particle-count');
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    c2.width = canvas.width;
+    c2.height = canvas.height;
 
     ctx = canvas.getContext('2d');
     ctx.fillStyle = '#eee';
     ctx.strokeStyle = '#999';
+
+    ctx2 = c2.getContext('2d');
+    ctx2.fillStyle = '#555';
+    ctx2.strokeStyle = '#555';
 
     drawLoop();
 };
@@ -138,3 +209,4 @@ window.addEventListener('DOMMouseScroll', scroll);
 window.addEventListener('mousemove', mousemove);
 window.addEventListener('mouseup', mouseup);
 window.addEventListener('mousedown', mousedown);
+window.addEventListener('keydown', keydown);
