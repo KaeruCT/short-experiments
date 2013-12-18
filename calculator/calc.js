5634238aff0e41941aa6e4395689dc39bf173373
Andres V <darklink898@gmail.com>
2013-12-17 18:46:14 -0600

    learned something new today

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,33 +1,51 @@
 var calculator = (function () {
     var operators = {
         '-': {
+            id: 0,
+            prec: 2,
+            assoc: 'l',
             fn: function (a, b) {
-                return parseFloat(a) - parseFloat(b);
+                return a - b;
             }
         },
         '+': {
+            id: 0,
+            prec: 2,
+            assoc: 'l',
             fn: function (a, b) {
-                return parseFloat(a) + parseFloat(b);
+                return a + b;
             }
         },
         '/': {
+            id: 1,
+            prec: 3,
+            assoc: 'l',
             fn: function (a, b) {
-                return parseFloat(a) / parseFloat(b);
+                return a / b;
             }
         },
         '*': {
+            id: 1,
+            prec: 3,
+            assoc: 'l',
             fn: function (a, b) {
-                return parseFloat(a) * parseFloat(b);
+                return a * b;
             }
         },
         '%': {
+            id: 1,
+            prec: 3,
+            assoc: 'l',
             fn: function (a, b) {
-                return parseFloat(a) % parseFloat(b);
+                return a % b;
             }
         },
         '^': {
+            id: 1,
+            prec: 4,
+            assoc: 'r',
             fn: function (a, b) {
-                return Math.pow(parseFloat(a), parseFloat(b));
+                return Math.pow(a, b);
             }
         }
     },
@@ -36,126 +54,153 @@ var calculator = (function () {
         pi: Math.PI
     };
 
-    function parseProgram (program) {
-        function replaceConstants(program) {
-            var c, val;
-            for (c in constants) {
-                program = program.replace(new RegExp('\\b(' + c + ')\\b', 'g'), constants[c]);
-            }
-            return program;
+    function replaceConstants(str) {
+        var c, val;
+        for (c in constants) {
+            str = str.replace(new RegExp('\\b(' + c + ')\\b', 'g'), constants[c]);
         }
-
-        function matchOperator(token) {
-            var i, op,
-                len = token.length;
-
-            for (op in operators) {
-                for (i = 0; i < len; i += 1) {
-
-                    if (token.charAt(i) === '(') {
-                        return -2;
-                    }
-
-                    if (token.charAt(i) === op) {
-                        return i;
-                    }
-                }
-            }
-            return -1;
-        }
-
-        function parseTokens(token) {
-            function parens(token) {
-                var i = 0, pcount = 0, j, char,
-                    len = token.length;
-
-                for (; i < len; i += 1) {
-                    char = token.charAt(i);
-
-                    if (char === '(') {
-                        for (j = i; j < len; j += 1) {
-                            char = token.charAt(j);
-                            if (char === '(') pcount += 1;
-                            if (char === ')') pcount -= 1;
-
-                            if (j > i && pcount === 0) {
-                                return token.substr(i + 1, j - i - 1);
-                            }
-                        }
-                    }
-                }
-                return token;
-            }
-
-            function parse(token) {
-                var ptoken, olen = 1,
-                    i, a, b, operator;
-
-                if (typeof token === "object") {
-                    return token;
-                }
-
-                i = matchOperator(token);
-
-                if (i === -2) {
-                    ptoken = parens(token);
-                    a = parse(ptoken);
-
-                    token = token.substr(ptoken.length + 1);
-                    i = matchOperator(token);
-
-                    if (i == -1) {
-                        return a;
-                    }
-                } else {
-                    if (i === -1) {
-                        return token;
-                    }
-
-                    a = token.substr(0, i);
-                }
-
-                operator = token.substr(i, olen);
-                b = token.substr(i + olen);
-                return {
-                    operator: operator,
-                    a: parse(a),
-                    b: parse(b)
-                };
-            }
-            return parse(token);
-        }
-
-        program = replaceConstants(program);
-        return parseTokens(program);
+        return str;
     }
 
-    function runProgram(tokens, debugFn) {
-        function runToken(token) {
-            var operator, a, b, result;
-            if (typeof token === "string") {
-                return token;
+    function tokenize(str) {
+        var tokens = [],
+            token = str[0],
+            i = 1;
+
+        for (; i < str.length; i += 1) {
+            if (token.match(/[\d\.]/) && str[i].match(/[\d\.]/)) {
+                token += str[i];
+            } else {
+                tokens.push(token);
+                token = str[i];
             }
-
-            a = runToken(token.a);
-            b = runToken(token.b);
-            operator = operators[token.operator];
-            result = operator.fn(a, b);
-
-            if (typeof debugFn === "function") {
-                debugFn(token.operator, a, b, result);
-            }
-
-            return result;
         }
 
-        return runToken(tokens);
+        tokens.push(token);
+        return tokens;
+    }
+
+    function getOperator(name) {
+        return operators[name];
+    }
+
+    function isNumber(str) {
+        return str.match(/\d+/);
+    }
+
+    function isOperator(str) {
+        return operators[str] !== undefined;
+    }
+
+    function parseProgram (str) {
+        var t, token, tokens,
+            top, o1, o2,
+            queue = [],
+            stack = [];
+
+        tokens = tokenize(replaceConstants(str.replace(/\s/g, '')));
+
+        for (t in tokens) {
+            token = tokens[t];
+            if (isNumber(token)) {
+                queue.unshift(token);
+                continue;
+            }
+
+            if (isOperator(token)) {
+                top = stack[stack.length-1];
+                while (top && isOperator(top)) {
+                    o1 = getOperator(token);
+                    o2 = getOperator(top);
+
+                    if ((o1.assoc === 'l' && o1.prec === o2.prec) || o1.prec < o2.prec) {
+                        queue.unshift(stack.pop());
+                    } else {
+                        break;
+                    }
+
+                    top = stack[stack.length-1];
+                }
+
+                stack.push(token);
+                continue;
+            }
+
+            if (token === '(') {
+                stack.push(token);
+                continue;
+            }
+
+            if (token === ')') {
+                top = stack[stack.length-1];
+                while (top && top !== '(') {
+                    queue.unshift(stack.pop());
+                    top = stack[stack.length-1];
+                }
+
+                if (!top) {
+                    return null;
+                }
+
+                stack.pop();
+                continue;
+            }
+        }
+
+        if (stack[0] === '(' || stack[stack.length-1] === ')') {
+            return null;
+        }
+
+        while (stack.length > 0) {
+            queue.unshift(stack.pop());
+        }
+
+        return queue.reverse();
+    }
+
+    function runProgram(rpn, debugFn) {
+        var t, token,
+            a, b, op, result,
+            stack = [];
+
+        for (t in rpn) {
+            token = rpn[t];
+
+            if (isNumber(token)) {
+                stack.push(token);
+                continue;
+            }
+
+            b = parseFloat(stack.pop());
+            a = parseFloat(stack.pop());
+            op = getOperator(token);
+
+            if (isNaN(b)) {
+                b = op.id;
+            } else if (isNaN(a)) {
+                a = op.id;
+            }
+
+            result = op.fn(a, b);
+            stack.push(result);
+
+            if (typeof debugFn === 'function') {
+                debugFn(token, a, b, result);
+            }
+        }
+
+        return stack.pop();
     }
 
     return {
         run: function (program, debugFn) {
-            var tokens = parseProgram(program);
-            return runProgram(tokens, debugFn);
+            var rpn = parseProgram(program);
+
+            if (rpn === null) {
+                return null;
+            }
+
+            return runProgram(rpn, debugFn);
         }
     };
 }());
